4ceebfd182244efd55e7d911f6d952fb1f60d276
BlueKrait7 <cu.18bcs2262@gmail.com>
2022-05-05 10:25:36 +0530

    Add project files.

---
+++ MusicDuct/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicDuct.Models
+{
+    public class ApplicationUser : DbContext
+    {
+        public ApplicationUser(DbContextOptions<ApplicationUser> options) : base(options)
+        {
+
+        }
+        public DbSet<User> UserRegistration { get; set; }
+    }
+}
---
+++ MusicDuct/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicDuct.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username..")]
+        [Display(Name = "UserName")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Pwd { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Confirm Password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Pwd")]
+        public string Confirmpwd { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
